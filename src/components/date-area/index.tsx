1b46987c9574bf7c4628b44e857347e565be1f12
Joseph Aje <ajejoseph22@gmail.com>
2021-02-21 00:47:45 +0100

    chore: created date area component

---
+++ src/components/date-area/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DateAreaWrapper = styled.div`
+  width: 25%;
+  background: pink;
+  float: left;
+  height: 100vh;
+`
+
+const DateArea: React.FC = () => {
+  return <DateAreaWrapper />
+}
+
+export default DateArea
