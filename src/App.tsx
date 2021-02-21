512f607ce9fb06d1cc35bf51bfd4344d4caf92be
Joseph Aje <ajejoseph22@gmail.com>
2021-02-21 01:00:44 +0100

    chore: added display area component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import DateArea from './components/date-area'
+import DisplayArea from './components/display-area'
 
 function App(): JSX.Element {
   return (
     <div>
       <DateArea />
+      <DisplayArea />
     </div>
   )
 }
---
+++ src/components/display-area/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DisplayAreaWrapper = styled.div`
+  height: 100vh;
+  background: greenyellow;
+`
+
+const DisplayArea: React.FC = () => {
+  return <DisplayAreaWrapper />
+}
+
+export default DisplayArea
